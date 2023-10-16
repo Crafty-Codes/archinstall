e1a0bb35a6fea235edc33a75bc19d3f9afc20bea
Crafty-Codes <104491949+Crafty-Codes@users.noreply.github.com>
2023-10-16 20:16:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,14 @@ rm -rf ~/.config/doom
 git clone https://github.com/Crafty-Codes/doom-emacs-config.git ~/.config/doom
 ~/.config/emacs/bin/doom sync
 
-git clone -b cherry-blossom https://github.com/flick0/dotfiles
-cd cherry-blossom
-chmod +x ./install
-./install
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S python-pywal python-desktop-entry-lib python-poetry python-build python-pillow
+sudo pacman -S bc blueberry bluez boost boost-libs coreutils dunst findutils fish fuzzel fzf gawk gnome-control-center ibus imagemagick libqalculate light networkmanager network-manager-applet nlohmann-json pavucontrol plasma-browser-integration playerctl procps ripgrep socat sox starship swaybg swayidle ttf-jetbrains-mono udev upower util-linux xorg-xrandr wget wireplumber wl-clipboard yad
+yay -S cava eww-wayland-git geticons gojq gtklock gtklock-playerctl-module gtklock-powerbar-module gtklock-userinfo-module hyprland-git python-material-color-utilities swww ttf-material-symbols-git xdg-desktop-portal-hyprland-git waybar-hyprland-git wlogout
+sudo pacman -S tesseract cliphist grim slurp
+git clone https://github.com/end-4/dots-hyprland.git dotfile
+cd dotfile
+./guided_install.sh 
